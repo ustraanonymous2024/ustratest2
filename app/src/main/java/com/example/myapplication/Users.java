5a2f8b50eed93fb3be03d206664a7a9a801556a6
ustraanonymous2024 <157122966+ustraanonymous2024@users.noreply.github.com>
2024-01-20 12:18:25 +0530

    final commit

---
+++ app/src/main/java/com/example/myapplication/Users.java
@@ -0,0 +1,19 @@
+package com.example.myapplication;
+
+public class Users {
+    String name;
+    public Users(){
+
+    }
+    public Users(String name){
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
